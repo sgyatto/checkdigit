9be2cbd436a48bd6dffc19ed4eb51c125c31da65
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-12-17 01:09:02 +0900

    Add Mod10W3::isValid(),Mod10W3::generateCode()

--- src/main/java/me/xeno/checkdigit/Mod10W3.java
+++ src/main/java/me/xeno/checkdigit/Mod10W3.java
@@ -1,11 +1,15 @@
 package me.xeno.checkdigit;
 
+/**
+ * This class calculates a check digit with Modulus 10, Weight 3:1 algorithm.
+ */
 public class Mod10W3 {
 
     /**
-     * Calculate CheckDigit
+     * Calculate a check digit.
      *
-     * @param numberStr target value
+     * @param numberStr The digits of string without check digit.
+     * @return A check digit.
      */
     public String calculateCheckDigit(String numberStr) {
 
@@ -33,5 +37,37 @@ public class Mod10W3 {
 
         long mod = sum % 10;
         return String.valueOf((mod == 0) ? 0 : 10 - mod);
+    }
+
+    /**
+     * Check the code.
+     *
+     * @param code The code with check digit.
+     * @return true or false.
+     */
+    public boolean isValid(String code) {
+
+        if (code == null || code.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        String checkDigit = code.substring(code.length() - 1);
+
+        return checkDigit.equals(calculateCheckDigit(code.substring(0, code.length() - 1)));
+    }
+
+    /**
+     * Generate a code with check digit.
+     *
+     * @param numberStr The digits of string without check digit.
+     * @return A code with check digit.
+     */
+    public String generateCode(String numberStr) {
+
+        if (numberStr == null || numberStr.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        return numberStr + calculateCheckDigit(numberStr);
     }
 }
--- src/test/java/me/xeno/checkdigit/Mod10W3Test.java
+++ src/test/java/me/xeno/checkdigit/Mod10W3Test.java
@@ -1,17 +1,17 @@
 package me.xeno.checkdigit;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class Mod10W3Test {
 
     @Test
-    void returnCheckDigit() {
+    void calculateCheckDigit() {
         Mod10W3 mod10w3 = new Mod10W3();
         assertEquals("8", mod10w3.calculateCheckDigit("4"));
         assertEquals("1", mod10w3.calculateCheckDigit("0425"));
         assertEquals("4", mod10w3.calculateCheckDigit("4912349"));
+        // when check digit is "0"
         assertEquals("0", mod10w3.calculateCheckDigit("20151119"));
     }
 
@@ -21,5 +21,32 @@ class Mod10W3Test {
         assertThrows(IllegalArgumentException.class, () -> mod10w3.calculateCheckDigit(null));
         assertThrows(IllegalArgumentException.class, () -> mod10w3.calculateCheckDigit(""));
         assertThrows(NumberFormatException.class, () -> mod10w3.calculateCheckDigit("0425あ"));
+    }
+
+    @Test
+    void isValid() {
+        Mod10W3 mod10w3 = new Mod10W3();
+        assertTrue(mod10w3.isValid("201511190"));
+        assertFalse(mod10w3.isValid("201501190"));
+    }
+
+    @Test
+    void isValidWithInvalidArgument() {
+        Mod10W3 mod10w3 = new Mod10W3();
+        assertThrows(IllegalArgumentException.class, () -> mod10w3.isValid(null));
+        assertThrows(IllegalArgumentException.class, () -> mod10w3.isValid(""));
+    }
+
+    @Test
+    void generateCode() {
+        Mod10W3 mod10w3 = new Mod10W3();
+        assertEquals("4901234567894", mod10w3.generateCode("490123456789"));
+    }
+
+    @Test
+    void generateCodeWithInvalidArgument() {
+        Mod10W3 mod10w3 = new Mod10W3();
+        assertThrows(IllegalArgumentException.class, () -> mod10w3.generateCode(null));
+        assertThrows(IllegalArgumentException.class, () -> mod10w3.generateCode(""));
     }
 }
